528a9224361695f739701f71214c66466a020bde
LemonJade <qazazqzw@gmail.com>
2017-03-03 14:14:59 +0800

    GG

--- Hello/Form1.cs
+++ Hello/Form1.cs
@@ -16,5 +16,10 @@ namespace Hello
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //GG
+        }
     }
 }
